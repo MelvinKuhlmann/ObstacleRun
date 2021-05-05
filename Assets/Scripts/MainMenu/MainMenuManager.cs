32ac4cabbbe6fed45b91dba2aa989d66f2b3d608
Sven van den Tweel <sven.vd.tweel@gmail.com>
2021-05-05 19:31:51 +0200

    Added a simple main menu

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+	public void StartGame()
+	{
+		//TODO set specific scenename here
+		Debug.Log("Start Game");
+		SceneManager.LoadScene(1);
+	}
+
+	public void LoadGame()
+	{
+		//TODO get last visited scene from playerpreferences
+		Debug.Log("Load Game");
+		SceneManager.LoadScene(1);
+	}
+
+	public void Credits()
+    {
+		//TODO show credits
+		Debug.Log("Credits");
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("Quit!");
+		Application.Quit();
+	}
+}
