42501925dae4e35504289277e74cb93b1ee98fc9
Sven van den Tweel <sven.vd.tweel@gmail.com>
2021-05-17 21:11:55 +0200

    Very simple enemy behaviour

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,31 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    void Start()
-    {
-       
-       // rigidBody = GetComponent<Rigidbody2D>();
-    }
+    public float speed = 5F;
 
-    // Update is called once per frame
+    private GameObject player;
+    private bool followPlayer = false;
+
     void Update()
     {
-        
+        if (followPlayer)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag(Tags.Player.ToString()))
         {
-           
+            player = other.gameObject;
+            followPlayer = true;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.gameObject.CompareTag(Tags.Player.ToString()))
+        {
+            player = null;
+            followPlayer = false;
+        }
     }
 }
