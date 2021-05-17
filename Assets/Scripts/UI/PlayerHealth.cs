05f763fb000523a1905d3a972bba82b746c1a117
Sven van den Tweel <sven.vd.tweel@gmail.com>
2021-05-17 17:32:18 +0200

    Added UI for a simple health system

---
+++ Assets/Scripts/UI/PlayerHealth.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health;
+    public int maxHealth;
+
+    public Image[] healthIcons;
+    public Sprite fullIcon;
+    public Sprite halfIcon;
+    public Sprite emptyIcon;
+
+    private void Update()
+    {
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        for (int i = 0; i < healthIcons.Length; i++)
+        {
+            if (i == Math.Floor(health) && i < Math.Ceiling(health))
+            {
+                healthIcons[i].sprite = halfIcon;
+            } 
+            else if (i < Math.Floor(health))
+            {
+                healthIcons[i].sprite = fullIcon;
+            }
+            else
+            {
+                healthIcons[i].sprite = emptyIcon;
+            }
+
+            if (i < maxHealth)
+            {
+                healthIcons[i].enabled = true;
+            } else
+            {
+                healthIcons[i].enabled = false;
+            }
+        }
+    }
+
+}
