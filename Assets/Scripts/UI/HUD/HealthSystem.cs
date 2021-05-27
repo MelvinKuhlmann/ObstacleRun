37534f430b9ca221ca79a90e50f0170fc236afb3
Sven van den Tweel <sven.vd.tweel@gmail.com>
2021-05-27 22:08:29 +0200

    Ended at https://www.youtube.com/watch?v=xWCJfE_sAXE : 23:00 Will continue tomorrow to complete the improved health system

---
+++ Assets/Scripts/UI/HUD/HealthSystem.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem
+{
+    public const int MAX_FRAGMENT_AMOUNT = 4;
+    public event EventHandler OnDamaged;
+    public event EventHandler OnHealed;
+
+    private List<Health> healthList;
+
+    public HealthSystem(int healthAmount)
+    {
+        healthList = new List<Health>();
+        for (int i = 0; i < healthAmount; i++)
+        {
+            Health health = new Health(4);
+            healthList.Add(health);
+        }
+    }
+
+    public List<Health> GetHealthList()
+    {
+        return healthList;
+    }
+
+    public void Damage(int damageAmount)
+    {
+        // Cycle through all the hearts starting from the end
+        for (int i = healthList.Count  - 1; i >= 0; i--)
+        {
+            Health health = healthList[i];
+            // Test if this health cannot absorb damageAmount
+            if (damageAmount > health.GetFragmentAmount())
+            {
+                // Health cannot absorb full damageAmount, damage current health and keep going to the next
+                damageAmount -= health.GetFragmentAmount();
+                health.Damage(health.GetFragmentAmount());
+            } else
+            {
+                // Health can absorb full damageAmount, absorb and break out of the cycle
+                health.Damage(damageAmount);
+                break;
+            }
+        }
+        if (OnDamaged != null) OnDamaged(this, EventArgs.Empty);
+    }
+
+    public void Heal(int healAmount)
+    {
+        // Cycle through all the hearts starting from the beginning
+        for (int i =0; i < healthList.Count; i++)
+        {
+            Health health = healthList[i];
+            int missingFragments = MAX_FRAGMENT_AMOUNT - health.GetFragmentAmount();
+            // Test if this health cannot absorb healAmount
+            if (healAmount > missingFragments)
+            {
+                // Health cannot absorb full healAmount, heal current health and keep going to the next
+                healAmount -= missingFragments;
+                health.Heal(missingFragments);
+            }
+            else
+            {
+                // Health can absorb full healAmount, absorb and break out of the cycle
+                health.Heal(healAmount);
+                break;
+            }
+        }
+        if (OnHealed != null) OnHealed(this, EventArgs.Empty);
+    }
+
+    // Represents a single heart
+    public class Health
+    {
+        private int fragments;
+
+        public Health(int fragments)
+        {
+            this.fragments = fragments;
+        }
+
+        public int GetFragmentAmount()
+        {
+            return fragments;
+        }
+
+        public void SetFragments(int fragments)
+        {
+            this.fragments = fragments;
+        }
+
+        public void Damage(int damageAmount)
+        {
+            if (damageAmount >= fragments)
+            {
+                fragments = 0;
+            } else
+            {
+                fragments -= damageAmount;
+            }
+        }
+
+        public void Heal(int healAmount)
+        {
+            if (fragments + healAmount > MAX_FRAGMENT_AMOUNT)
+            {
+                fragments = MAX_FRAGMENT_AMOUNT;
+            } else
+            {
+                fragments += healAmount;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/HUD/HealthVisual.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=xWCJfE_sAXE : 23:00
+public class HealthVisual : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite health0Sprite;
+    [SerializeField]
+    private Sprite health1Sprite;
+    [SerializeField]
+    private Sprite health2Sprite;
+    [SerializeField]
+    private Sprite health3Sprite;
+    [SerializeField]
+    private Sprite health4Sprite;
+
+    private List<HealthImage> healthImageList;
+    private HealthSystem healthSystem;
+
+    private void Awake()
+    {
+        healthImageList = new List<HealthImage>();
+    }
+
+    private void Start()
+    {
+        HealthSystem healthSystem = new HealthSystem(4);
+        SetHealthSystem(healthSystem);
+    }
+
+    public void SetHealthSystem(HealthSystem healthSystem)
+    {
+        this.healthSystem = healthSystem;
+
+        List<HealthSystem.Health> healthList = healthSystem.GetHealthList();
+        Vector2 healthAnchoredPosition = new Vector2(0, 0);
+        for (int i = 0; i < healthList.Count; i++)
+        {
+            HealthSystem.Health health = healthList[i];
+            CreateHealthImage(healthAnchoredPosition).SetHealthFragments(health.GetFragmentAmount());
+            healthAnchoredPosition += new Vector2(40, 0);
+        }
+
+        healthSystem.OnDamaged += HealthSystem_OnDamaged;
+        healthSystem.OnHealed += HealthSystem_OnHealed;
+    }
+
+    private void HealthSystem_OnDamaged(object sender, System.EventArgs e)
+    {
+        // Health system was damaged
+        RefreshAllHealth();
+    }
+
+    private void HealthSystem_OnHealed(object sender, System.EventArgs e)
+    {
+        // Health system was healed
+        RefreshAllHealth();
+    }
+
+    private void RefreshAllHealth()
+    {
+        List<HealthSystem.Health> healtList = healthSystem.GetHealthList();
+        for (int i = 0; i < healtList.Count; i++)
+        {
+            HealthImage healthImage = healthImageList[i];
+            HealthSystem.Health health = healtList[i];
+            healthImage.SetHealthFragments(health.GetFragmentAmount());
+        }
+    }
+
+    private HealthImage CreateHealthImage(Vector2 anchoredPosition)
+    {
+        GameObject healthGameObject = new GameObject("Health", typeof(Image));
+        // Set as child of this transform
+        // heartGameObject.transform.parent = transform;
+        healthGameObject.transform.SetParent(transform);
+        healthGameObject.transform.localPosition = Vector3.zero;
+
+        // Locate and Size of object
+        healthGameObject.GetComponent<RectTransform>().anchoredPosition = anchoredPosition;
+        healthGameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(40, 40);
+
+        // Set heart sprite
+        Image heartImageUI = healthGameObject.GetComponent<Image>();
+        heartImageUI.sprite = health0Sprite;
+
+        HealthImage heartImage = new HealthImage(this, heartImageUI);
+        healthImageList.Add(heartImage);
+        return heartImage;
+    }
+
+    // Represents a single heart
+    public class HealthImage
+    {
+        private HealthVisual healthVisual;
+        private Image healthImage;
+
+        public HealthImage(HealthVisual healthVisual, Image healthImage)
+        {
+            this.healthVisual = healthVisual;
+            this.healthImage = healthImage;
+        }
+
+        public void SetHealthFragments(int fragments)
+        {
+            switch (fragments)
+            {
+                case 0:
+                    healthImage.sprite = healthVisual.health0Sprite;
+                    break;
+                case 1:
+                    healthImage.sprite = healthVisual.health1Sprite;
+                    break;
+                case 2:
+                    healthImage.sprite = healthVisual.health2Sprite;
+                    break;
+                case 3:
+                    healthImage.sprite = healthVisual.health3Sprite;
+                    break;
+                case 4:
+                    healthImage.sprite = healthVisual.health4Sprite;
+                    break;
+            }
+        }
+    }
+}
